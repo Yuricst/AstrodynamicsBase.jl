e98ba373aa905b15cb7c8e00b3a11fbcdd75665d
yuri@tux <yuri.shimane@gmail.com>
2022-09-15 17:59:08 -0400

    overlap removed

--- src/trigonometry.jl
+++ src/trigonometry.jl
@@ -12,26 +12,3 @@ Safe acos function
 function asin_safe(val::Real)
     return asin(max(-1.0, min(1.0, val)))
 end
-
-
-"""
-    mod_custom(a, n)
-
-Custom modulo function
-Ref: https://stackoverflow.com/questions/1878907/how-can-i-find-the-difference-between-two-angles
-"""
-function mod_custom(a, n)
-    return a - floor(a / n) * n
-end
-
-
-"""
-    angle_difference(ϕ_fwd::Real, ϕ_bck::Real)
-
-Compute angle difference for periodic angles between 0 and 2π
-"""
-function angle_difference(ϕ_fwd::Real, ϕ_bck::Real)
-    # modulo based
-    dϕ = mod_custom((ϕ_bck - ϕ_fwd + π), 2π) - π
-    return dϕ
-end
